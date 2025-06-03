c20d51fb38bc4c96919574606abdb62efb0cfbec
lokeshloki3 <lokesh1992p@gmail.com>
2025-06-04 01:29:32 +0530

    Interests page updated

--- src/components/Interests.jsx
+++ src/components/Interests.jsx
@@ -1,23 +1,32 @@
-import React from 'react'
-
-const Interests = ({ data, setData }) => {
+export default function Interests({ data, setData, errors }) {
   const { interests } = data;
-
   const handleChange = (e) => {
     setData((prevState) => ({
       ...prevState,
-      interests: e.target.checked ?
-        [...prevState.interests, e.target.name]
-        : prevState.interests.filter((item) => item != e.target.name)
-    }))
-  }
+      interests: e.target.checked
+        ? [...prevState.interests, e.target.name]
+        : prevState.interests.filter((item) => item !== e.target.name),
+    }));
+  };
+
+  // console.log("interests", interests);
 
   return (
     <div>
       <div>
         <input
-          type='checkbox'
-          name='java'
+          type="checkbox"
+          name="javaScript"
+          checked={interests.includes("javaScript")}
+          onChange={handleChange}
+        />
+        <label>JavaScript</label>
+      </div>
+
+      <div>
+        <input
+          type="checkbox"
+          name="java"
           checked={interests.includes("java")}
           onChange={handleChange}
         />
@@ -26,25 +35,14 @@ const Interests = ({ data, setData }) => {
 
       <div>
         <input
-          type='checkbox'
-          name='javaScript'
-          checked={interests.includes("javaScript")}
-          onChange={handleChange}
-        />
-        <label>javaScript</label>
-      </div>
-
-      <div>
-        <input
-          type='checkbox'
-          name='music'
+          type="checkbox"
+          name="music"
           checked={interests.includes("music")}
           onChange={handleChange}
         />
-        <label>music</label>
+        <label>Music</label>
       </div>
+      {/* {errors.interests && <span className="errors">{errors.interests}</span>} */}
     </div>
-  )
+  );
 }
-
-export default Interests
