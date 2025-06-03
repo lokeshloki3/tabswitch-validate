ce3dd48c5a982f379020c4f4fa5d43f745037c01
lokeshloki3 <lokesh1992p@gmail.com>
2025-06-03 21:14:14 +0530

    Profile page created

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
 
 const Profile = ({ data, setData }) => {
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setData((prevData) => ({
+      ...prevData, [name]: value,
+    }))
+  }
+
   return (
     <div>
-
+      <div className='fields'>
+        {/* htmlFor is for ID */}
+        <label htmlFor='name'>Name</label>
+        <input
+          type='text'
+          name='name'
+          id='name'
+          value={data.name}
+          onChange={handleChange}
+        />
+      </div>
+      <div className='fields'>
+        <label htmlFor='age'>Age</label>
+        <input
+          type='number'
+          name='age'
+          id='age'
+          value={data.age}
+          onChange={handleChange}
+        />
+      </div>
+      <div className='fields'>
+        <label htmlFor='email'>Email</label>
+        <input
+          type='text'
+          name='email'
+          id='email'
+          value={data.email}
+          onChange={handleChange}
+        />
+      </div>
     </div>
   )
 }
